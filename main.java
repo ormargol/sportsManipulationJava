c0b4fb03c4bb5fa3ca4fa44c33d4cc13e6ca0ffb
Orr Margaliot <ormargol@gmail.com>
2017-11-15 12:03:48 +0200

    compilation works
    Signed-off-by: Orr Margaliot <ormargol@gmail.com>

--- main.java
+++ main.java
@@ -1,12 +1,33 @@
 import java.lang.*;
 
-class sportsManipulation {
-  private static final int TEAMS_NUM = 8;
-  private static final int GROUP_SIZE = 4;
-  private static final int GROUPS_NUM = TEAMS_NUM / GROUP_SIZE;
-  private static final int LOG_LVL_DEBUG = 2;
-  private static final int LOG_LVL_TEST = 1;
-  private static int debug_level;
+class team {
+  int id;
+  int score;
+
+  team(int n_id) {
+    id = n_id;
+    score = 0;
+  }
+}
+
+class group {
+  int id;
+  team[] teams;
+
+  group(int n_id, int size) {
+    id = n_id;
+    teams = new team[size];
+  }
+}
+
+class league {
+
+  static final int TEAMS_NUM = 8;
+  static final int GROUP_SIZE = 4;
+  static final int GROUPS_NUM = TEAMS_NUM / GROUP_SIZE;
+  static final int LOG_LVL_DEBUG = 2;
+  static final int LOG_LVL_TEST = 1;
+  static int debug_level;
 
   public static void LOG(final int lvl, final String in, final Object... args) {
     if (lvl <= debug_level) {
@@ -22,83 +43,60 @@ class sportsManipulation {
     LOG(LOG_LVL_TEST, in, args);
   }
 
-  class team {
-    int id;
-    int score;
+  group[] groups;
+  Boolean[][] strength_graph;
+  Boolean[][] positive_manipulators;
+  int manipulated_team_id;
 
-    team(int n_id) {
-      id = n_id;
-      score = 0;
-    }
+  league() {
+    groups = new group[GROUPS_NUM];
+    strength_graph = new Boolean[TEAMS_NUM][TEAMS_NUM];
+    positive_manipulators = new Boolean[TEAMS_NUM][TEAMS_NUM];
   }
 
-  class group {
-    int id;
-    team[] teams;
-
-    group(int n_id) {
-      id = n_id;
-      teams = new team[GROUP_SIZE];
-    }
-  }
-
-  class league {
-    group[] groups;
-    Boolean[][] strength_graph;
-    Boolean[][] positive_manipulators;
-    int manipulated_team_id;
-
-    league(int n_manipulated_team_id) {
-      manipulated_team_id = n_manipulated_team_id;
-      groups = new group[GROUPS_NUM];
-      strength_graph = new Boolean[TEAMS_NUM][TEAMS_NUM];
-      positive_manipulators = new Boolean[TEAMS_NUM][TEAMS_NUM];
-    }
-  }
-
-  public static int algorithm_init(league lg, int argc, String[] argv) {
+  public int algorithm_init(int argc, String[] argv) {
       int g, t1, t2;
       for (g = 0; g < GROUPS_NUM; g++) {
-          lg.groups[g] = new group(g + 1);
+          groups[g] = new group(g + 1, GROUP_SIZE);
           for (t1 = 0; t1 < GROUP_SIZE; t1++) {
-              lg.groups[g].teams[t1] = new team(g * GROUP_SIZE + t1);
+              groups[g].teams[t1] = new team(g * GROUP_SIZE + t1);
           }
       }
       for (t1 = 0; t1 < TEAMS_NUM; t1++) {
           for (t2 = 0; t2 < TEAMS_NUM; t2++) {
               if (t2 >= t1 + 1) {
-                  lg.strength_graph[t1][t2] = true;// t1 beats t2
+                  strength_graph[t1][t2] = true;// t1 beats t2
               } else {
-                  lg.strength_graph[t1][t2] = false;
+                  strength_graph[t1][t2] = false;
               }
-              lg.positive_manipulators[t1][t2] = false;
+              positive_manipulators[t1][t2] = false;
           }
       }
       if (argc > 1) {
           debug_level = Integer.parseInt(argv[1]);
           if (argc > 2) {
-              lg.manipulated_team_id = Integer.parseInt(argv[2]);
+              manipulated_team_id = Integer.parseInt(argv[2]);
               int i, j;
               for (i = 3; i < argc; i++) {
                   j = Integer.parseInt(argv[i]);
-                  lg.positive_manipulators[j][lg.manipulated_team_id] = true;
-                  LOGD("team %d will manipulate for win of team %d\n", j, lg.manipulated_team_id);
+                  positive_manipulators[j][manipulated_team_id] = true;
+                  LOGD("team %d will manipulate for win of team %d\n", j, manipulated_team_id);
               }
           }
       }
       return 0;
   }
 
-  public static Boolean game_winner_first_win(league lg, int tm1, int tm2) {
-      if (lg.positive_manipulators[tm1][tm2] == true) {
+  public Boolean game_winner_first_win(int tm1, int tm2) {
+      if (positive_manipulators[tm1][tm2] == true) {
           LOGD("%d wins %d due to manipulation\n", tm2, tm1);
           return false;
       }
-      if (lg.positive_manipulators[tm2][tm1] == true) {
+      if (positive_manipulators[tm2][tm1] == true) {
           LOGD("%d wins %d due to manipulation\n", tm1, tm2);
           return true;
       }
-      if (lg.strength_graph[tm1][tm2] == true) {
+      if (strength_graph[tm1][tm2] == true) {
           LOGD("%d wins %d due to strength\n", tm1, tm2);
           return true;
       }
@@ -106,7 +104,7 @@ class sportsManipulation {
       return false;
   }
 
-  public static Boolean first_team_best_score(league lg, team tm1, team tm2) {
+  public Boolean first_team_best_score(team tm1, team tm2) {
       if (tm1.score > tm2.score) {
           LOGD("%d before %d due to score\n", tm1.id, tm2.id);
           return true;
@@ -115,15 +113,15 @@ class sportsManipulation {
           LOGD("%d before %d due to score\n", tm2.id, tm1.id);
           return false;
       }
-      if (lg.positive_manipulators[tm1.id][tm2.id] == true) {
+      if (positive_manipulators[tm1.id][tm2.id] == true) {
           LOGD("%d before %d due to manipulation\n", tm2.id, tm1.id);
           return false;
       }
-      if (lg.positive_manipulators[tm2.id][tm1.id] == true) {
+      if (positive_manipulators[tm2.id][tm1.id] == true) {
           LOGD("%d before %d due to manipulation\n", tm1.id, tm2.id);
           return true;
       }
-      if (lg.strength_graph[tm1.id][tm2.id] == true) {
+      if (strength_graph[tm1.id][tm2.id] == true) {
           LOGD("%d before %d due to strength\n", tm1.id, tm2.id);
           return true;
       }
@@ -131,17 +129,17 @@ class sportsManipulation {
       return false;
   }
 
-  public static int calculate_groups_scores(league lg) {
+  public int calculate_groups_scores() {
       int g, t1, t2;
       team temp;
       for (g = 0; g < GROUPS_NUM; g++) {
           LOGD("GROUP %d - calculate winners:\n", g);
           for (t1 = 0; t1 < GROUP_SIZE; t1++) {
               for (t2 = t1 + 1; t2 < GROUP_SIZE; t2++) {
-                  if (game_winner_first_win(lg, lg.groups[g].teams[t1].id, lg.groups[g].teams[t2].id) == true) {
-                      lg.groups[g].teams[t1].score += 3;
+                  if (game_winner_first_win(groups[g].teams[t1].id, groups[g].teams[t2].id) == true) {
+                      groups[g].teams[t1].score += 3;
                   } else {
-                      lg.groups[g].teams[t2].score += 3;
+                      groups[g].teams[t2].score += 3;
                   }
               }
           }
@@ -149,39 +147,39 @@ class sportsManipulation {
           LOGD("calculate table order:\n");
           for (t1 = 0; t1 < GROUP_SIZE; t1++) {
               for (t2 = t1 + 1; t2 < GROUP_SIZE; t2++) {
-                  if (first_team_best_score(lg, lg.groups[g].teams[t2], lg.groups[g].teams[t1])) {
-                      temp = lg.groups[g].teams[t1];
-                      lg.groups[g].teams[t1] = lg.groups[g].teams[t2];
-                      lg.groups[g].teams[t2] = temp;
+                  if (first_team_best_score(groups[g].teams[t2], groups[g].teams[t1])) {
+                      temp = groups[g].teams[t1];
+                      groups[g].teams[t1] = groups[g].teams[t2];
+                      groups[g].teams[t2] = temp;
                   }
               }
           }
           LOGD("\n");
           LOGD("final table:\n");
           for (t1 = 0; t1 < GROUP_SIZE; t1++) {
-              LOGD("TEAM %d: score=%d\n", lg.groups[g].teams[t1].id, lg.groups[g].teams[t1].score);
+              LOGD("TEAM %d: score=%d\n", groups[g].teams[t1].id, groups[g].teams[t1].score);
           }
           LOGD("\n");
       }
       return 0;
   }
 
-  public static Boolean calculate_tree(league lg) {
+  public Boolean calculate_tree() {
       team[] tree_teams = new team[GROUPS_NUM * 2];
       int g, t = 0;
       for (g = 0; g < GROUPS_NUM; g++, t++) {
           if (g %2 == 0) {
-              tree_teams[t] = lg.groups[g].teams[0];
-              tree_teams[GROUPS_NUM * 2 - t - 1] = lg.groups[g].teams[1];
+              tree_teams[t] = groups[g].teams[0];
+              tree_teams[GROUPS_NUM * 2 - t - 1] = groups[g].teams[1];
           } else {
-              tree_teams[t] = lg.groups[g].teams[1];
-              tree_teams[GROUPS_NUM * 2 - t - 1] = lg.groups[g].teams[0];
+              tree_teams[t] = groups[g].teams[1];
+              tree_teams[GROUPS_NUM * 2 - t - 1] = groups[g].teams[0];
           }
       }
       int left_teams;
       for (left_teams = GROUPS_NUM * 2; left_teams > 1; left_teams /= 2) {
           for (t = 0; t < left_teams / 2; t++) {
-              if (game_winner_first_win(lg, tree_teams[2 * t].id, tree_teams[2 * t + 1].id) == true) {
+              if (game_winner_first_win(tree_teams[2 * t].id, tree_teams[2 * t + 1].id) == true) {
                   tree_teams[t] = tree_teams[2 * t];
               } else {
                   tree_teams[t] = tree_teams[2 * t + 1];
@@ -190,25 +188,25 @@ class sportsManipulation {
           LOGD("\n");
       }
       LOGD("%d\n", tree_teams[0].id);
-      if (tree_teams[0].id == lg.manipulated_team_id) {
+      if (tree_teams[0].id == manipulated_team_id) {
           return true;
       } else {
           return false;
       }
   }
 
-  public static Boolean try_swap_two_firsts_on_tm_group(league lg) {
+  public Boolean try_swap_two_firsts_on_tm_group() {
       return false;
   }
 
-  public static int algorithm_execute(league lg) {
-      calculate_groups_scores(lg);
-      if (calculate_tree(lg)) {
+  public int algorithm_execute() {
+      calculate_groups_scores();
+      if (calculate_tree()) {
           LOGT("yes\n");
           return 0;
       }
-      if (try_swap_two_firsts_on_tm_group(lg)) {
-          if (calculate_tree(lg)) {
+      if (try_swap_two_firsts_on_tm_group()) {
+          if (calculate_tree()) {
               LOGT("yes\n");
               return 0;
           }
@@ -216,11 +214,13 @@ class sportsManipulation {
       LOGT("no\n");
       return 0;
   }
+}
 
+class sportsManipulation {
   public static int main(int argc, String[] argv) {
-      league lg;
-      algorithm_init(lg, argc, argv);
-      algorithm_execute(lg);
+      league lg = new league();
+      lg.algorithm_init(argc, argv);
+      lg.algorithm_execute();
       return 0;
   }
 }
